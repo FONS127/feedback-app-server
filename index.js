c95a5acc79e14693b49580ecca51982cf505c35a
Eissa <O.Eissa@Dell.com>
2022-07-19 16:22:11 +0200

    Added Mongo(ose)

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 //import express from 'express'; ES2015/ES6 Modules way
 const express = require("express"); //Common modules way
+
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+
+require("./models/User");
 require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Home");
+  res.send(keys.mongoURI);
 });
 
 require("./routes/authRoutes")(app);
