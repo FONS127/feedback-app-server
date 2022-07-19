45984c35e1223695affa4135c3be46f5bde2de94
Eissa <O.Eissa@Dell.com>
2022-07-19 11:42:32 +0200

    FirstS

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express"); //Common modules
+//import express from 'express'; ES2015/ES6 Modules
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there2", bye: "later" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
