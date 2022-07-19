819a046b480f9d7d8b2439fe38a151501cdc7782
Eissa <O.Eissa@Dell.com>
2022-07-19 14:23:18 +0200

    PreMongo

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require("express"); //Common modules
-//import express from 'express'; ES2015/ES6 Modules
+//import express from 'express'; ES2015/ES6 Modules way
+const express = require("express"); //Common modules way
+require("./services/passport");
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there3", bye: "later" });
+  res.send("Home");
 });
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
